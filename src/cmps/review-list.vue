dfcfacd07b934f8590fcfdeeac0e1483dce26898
Noa Reisman <noareisman10@gmail.com>
2021-03-17 14:49:21 +0200

    temp

---
+++ src/cmps/review-list.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <h1>Reviews</h1>
+    <ul class="review-card-grid">
+      <li v-for="review in reviews" :key="review._id">
+        <stay-preview :review="review"></stay-preview>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import stayPreview from "../cmps/review-preview.vue";
+
+export default {
+  name: "review-list",
+  props: {
+    reviews: Array,
+  },
+  methods: {
+  },
+  components: {
+    stayPreview,
+  },
+};
+</script>
--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -8,9 +8,14 @@
 <script>
 
 import tripSetting from '../cmps/trip-settings.vue'
+import reviewList from '../cmps/reviewList.vue'
+import { stayService } from "../services/stay.service.js";
+import reviewList from "@/cmps/review-list.vue";
+
 export default {
   components:{
-    tripSetting
+    tripSetting,
+    reviewList
 
   }
   }
@@ -19,3 +24,84 @@ export default {
 <style>
 
 </style>
+
+
+<template>
+  <section
+    v-if="toy"
+    class="toy-details main-container flex align-center space-evenly"
+    :style="direction"
+  >
+    <div class="info-container flex column space-between">
+      <h2>{{ toy.name }}</h2>
+      <div class="details-info">
+        <!-- <h3>id:</h3>
+        <h4>{{ toy._id }}</h4> -->
+        <h3>{{ $t("message.price") }}</h3>
+        <h4>{{ formattedPrice }}</h4>
+        <h3>{{ $t("message.type") }}:</h3>
+        <h4>{{ toy.type }}</h4>
+        <h3>{{ $t("message.in-stock") }}:</h3>
+        <h4>{{ toy.inStock }}</h4>
+        <h3>{{ $t("message.created-at") }}:</h3>
+        <h4>{{ date }}</h4>
+      </div>
+      <div class="flex space-between align-center">
+        <router-link class="btn back" to="/toy">{{
+          $t("message.back")
+        }}</router-link>
+        <button @click="addReview" class="btn confirm">{{ $t("message.add-review") }}</button>
+      </div>
+    </div>
+    <div class="img-container">
+      <img class="details-img" :src="imgSrc" />
+    </div>
+    <review-list v-if="reviews" class="review-accordion" :reviews="reviews" :pageSize="3"/>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: "toyDetails",
+  data() {
+    return {
+      toy: null,
+      reviews : null
+    };
+  },
+  created() {
+    const toyId = this.$route.params.toyId;
+    toyService.getById(toyId).then((toy) => (this.toy = toy));
+    this.setReviews(toyId);
+  },
+  methods: {
+    addReview() {
+      this.$router.push("/details/review/" + this.toy._id);
+    },
+    async setReviews(toyId){
+      this.reviews = await reviewService.query({toyId})
+    }
+  },
+  computed: {
+    date() {
+      return new Date(this.toy.createdAt).toLocaleDateString("he-IS");
+    },
+    imgSrc() {
+      return require(`@/assets/imgs/${this.toy.url}`);
+    },
+    direction() {
+      return this.$store.getters.direction;
+    },
+    formattedPrice() {
+      const { locale, currency, multiplier } = this.$store.getters.getLocale;
+      const price = this.toy.price * multiplier;
+      return (
+        "locale", price.toLocaleString(locale, { style: "currency", currency })
+      );
+    },
+
+  },
+
+};
+</script>
