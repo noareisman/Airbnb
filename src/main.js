a2283b19a73f611fd3e9e2348a9b4f821ae1f95d
Your Name <your_address_email@domain.com>
2021-03-17 18:59:27 +0200

    stay preview

--- src/main.js
+++ src/main.js
@@ -5,11 +5,18 @@ import  router  from './router'
 import store from './store'
 import '../style/scss/main.scss'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  ElementUI,
   render: h => h(app)
 }).$mount('#app')
  
--- src/views/stay-app.vue
+++ src/views/stay-app.vue
@@ -1,5 +1,5 @@
 <template>
-<section> 
+<section class="main-layout"> 
   
   <stay-filter/>
   <h1> stay app </h1>
