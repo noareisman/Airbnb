274443ed33eb8357751ea6bff597436be09ebfff
Your Name <your_address_email@domain.com>
2021-03-18 17:20:06 +0200

    date picker

--- src/cmps/hero.vue
+++ src/cmps/hero.vue
@@ -4,43 +4,7 @@
      <div
         class="filter-container flex justify-center align-center scroll-serarch"
       >
-        <div class="wrapper flex">
-          <span class="box">
-            <div class="location">
-              <span class="title">Location</span>
-              <input
-                type="text"
-                placeholder="where are you going?"
-                class="desc"
-              />
-            </div>
-          </span>
-          <span class="box">
-            <div class="location">
-              <span class="title">Dates</span>
-              <input
-                type="text"
-                placeholder="where are you going?"
-                class="desc"
-              />
-            </div>
-          </span>
-          <span class="box">
-            <div class="guests">
-              <span class="title">Guests</span>
-              <input
-                type="text"
-                placeholder="where are you going?"
-                class="desc"
-              />
-            </div>
-          </span>
-          <div class="search-icon">
-            <div class="btn-search">
-              <img src="../assets/imgs/icons/icon-search.png">
-            </div>
-          </div>
-        </div>
+       <stay-filter/>
       </div>
       <div class="h1-container">
         <h1 class="hero-header">Choose</h1>
@@ -54,8 +18,13 @@
 </template>
 
 <script>
+import stayFilter from '../cmps/stay-filter.vue'
 export default {
 
+
+components:{
+  stayFilter
+}
 }
 </script>
 
--- src/cmps/stay-filter.vue
+++ src/cmps/stay-filter.vue
@@ -1,15 +1,64 @@
 <template>
   <section>
-      <h1>stay filter</h1>
+    <div class="wrapper flex">
+      <span class="box">
+        <div class="location">
+          <span class="title">Location</span>
+          <input
+            v-model="filterBy.location"
+            type="text"
+            placeholder="where are you going?"
+            class="desc"
+          />
+        </div>
+      </span>
+      <span class="box">
+        <div class="location">
+          <span class="title">Dates</span>
+          <date-picker @pick="datePick" />
+        </div>
+      </span>
+      <span class="box">
+        <div class="guests">
+          <span class="title">Guests</span>
+          <input type="text" placeholder="where are you going?" class="desc" />
+        </div>
+      </span>
+      <div class="search-icon">
+        <div class="btn-search">
+          <img src="../assets/imgs/icons/icon-search.png" />
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import datePicker from "../cmps/date-picker.vue";
 export default {
-
-}
+  name: "stayFilter",
+  data() {
+    return {
+      filterBy: {
+        location: "",
+        startDate: "",
+        endDate: "",
+        guests: 0,
+      },
+    };
+  },
+  methods: {
+    datePick(date) {
+      this.filterBy.startDate = date[0];
+      this.filterBy.endDate = date[1];
+      console.log(date);
+    },
+  },
+  components: {
+    datePicker,
+  },
+};
 </script>
 
 <style>
-
 </style>
