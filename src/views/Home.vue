097efeef8d2819b79c74bd41f0c23bc206b6f45f
Your Name <your_address_email@domain.com>
2021-03-18 11:06:51 +0200

    about

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,62 +1,46 @@
 <template>
   <section class="home main-layout">
     <section class="hero full main-layout">
-      <div
-        class="filter-container flex justify-center align-center scroll-serarch"
-      >
-        <div class="wrapper flex">
-          <span class="box">
-            <div class="location">
-              <span class="title">Location</span>
-              <input
-                type="text"
-                placeholder="where are you going?"
-                class="desc"
-              />
-            </div>
-          </span>
-          <span class="box">
-            <div class="location">
-              <span class="title">Dates</span>
-              <input
-                type="text"
-                placeholder="where are you going?"
-                class="desc"
-              />
-            </div>
-          </span>
-          <span class="box">
-            <div class="guests">
-              <span class="title">Guests</span>
-              <input
-                type="text"
-                placeholder="where are you going?"
-                class="desc"
-              />
-            </div>
-          </span>
-          <div class="search-icon">
-            <div class="btn-search">
-              <img src="../assets/imgs/icons/icon-search.png">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="h1-container">
-        <h1 class="hero-header">Choose</h1>
-        <h1 class="hero-header-2">
-          Where To Live <span class="dot">. </span>
-        </h1>
-      </div>
+      <hero />
     </section>
+    <h2 class="location-header">Explore locations</h2>
+    <div class="card-container">
+      <div class="location">
+        <img
+          class="explore-img"
+          src="../assets/imgs/explore-location/barcelona.jpg"
+        />
+        <p class="location-txt">Barcelona</p>
+      </div>
+      <div class="location">
+        <img
+          class="explore-img"
+          src="../assets/imgs/explore-location/new-york.jpg"
+        />
+        <p class="location-txt">New York</p>
+      </div>
+      <div class="location">
+        <img
+          class="explore-img"
+          src="../assets/imgs/explore-location/paris.jpg"
+        />
+        <p class="location-txt">Paris</p>
+      </div>
+    </div>
+    <about />
   </section>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import hero from "../cmps/hero.vue";
+import about from "../cmps/about.vue";
 export default {
   name: "home",
-  components: {},
+  components: {
+    hero,
+    about,
+  },
 };
 </script>
