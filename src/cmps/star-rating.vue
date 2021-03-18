7b8c796a50e24400dbdb275cff50493adf45dac9
Noa Reisman <noareisman10@gmail.com>
2021-03-18 11:34:04 +0200

    star rating component

---
+++ src/cmps/star-rating.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <span>⭐{{ reviews.avgRate }}({{ reviews.length }})</span>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "star-rating",
+  props: {
+    reviews: Array,
+  },
+};
+</script>
--- src/views/stay-details.vue
+++ src/views/stay-details.vue
@@ -1,26 +1,23 @@
 <template>
-  <section
-    v-if="stay"
-    class="stay-details-container"
-  >
+  <section v-if="stay" class="stay-details-container">
     <h1>{{ stay.summary }}</h1>
     <!-- TODO: finish routerLink -->
     <div>
-    <div></div>
-    <router-link to="/stay/:id/:location?">{{stay.loc.address}}</router-link>
+      <star-rating :reviews="stay.reviews" />
+      <router-link to="/stay/:id/:location?">{{stay.loc.address}}</router-link>
     </div>
     <div>
       <button class="share-btn">Share</button>
       <button class="save-btn">Save</button>
     </div>
-      <ul class="stay-details-img-grid-container">
-        <li v-for="img in stay.imgUrls" :key="img">
-          <img
-            class="stay-img"
-            :src="require(`@/assets/imgs/airbnb-imgs/${img}.jpg`)"
-          />
-        </li>
-      </ul>
+    <ul class="stay-details-img-grid-container">
+      <li v-for="img in stay.imgUrls" :key="img">
+        <img
+          class="stay-img"
+          :src="require(`@/assets/imgs/airbnb-imgs/${img}.jpg`)"
+        />
+      </li>
+    </ul>
     <!-- <h1>{{ stay.name }} hosted by {{ host }}</h1> -->
     <!-- <p>Up to {{ guestAmount }}</p> -->
 
@@ -32,14 +29,15 @@
 <script>
 import tripSettings from "../cmps/trip-settings.vue";
 import reviewList from "../cmps/review-list.vue";
+import starRating from "../cmps/star-rating.vue";
 import { stayService } from "../services/stay.service.js";
 
 export default {
   name: "stay-details",
   data() {
     return {
-      stay:null
-    }
+      stay: null,
+    };
   },
   computed: {
     guestAmount() {
@@ -53,13 +51,14 @@ export default {
   created() {
     const _id = this.$route.params.id;
     stayService.getById(_id).then((stay) => {
-    this.stay = stay;
+      this.stay = stay;
     });
   },
   components: {
     tripSettings,
     reviewList,
-    stayService
+    stayService,
+    starRating,
   },
 };
 </script>
