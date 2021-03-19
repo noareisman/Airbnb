05d7c44bf2a44ee5797e01cb6ec3cd0633faeaab
Your Name <your_address_email@domain.com>
2021-03-19 17:31:22 +0200

    bug?

--- src/cmps/user-dashboard.vue
+++ src/cmps/user-dashboard.vue
@@ -1,14 +1,19 @@
 <template>
-  <section>
-    i am user dashboard
+  <section class="preview-card space-preview main-layout ">
     <ul> 
-      <li class="userLikes" v-for="(stayLiked, idx) in likes " :key="idx"> {{stayLiked.name}} </li>
+      <h1>{{user.fullname}} Favorites </h1>
+      <li class="userLikes" v-for="(stayLiked, idx) in likes " :key="idx"> 
+        <stay-preview :stay="stayLiked" class="preview-deshboard"/>
+
+      </li>
     </ul>
   </section>
 </template>
 
 <script>
+import stayPreview from './stay-preview.vue';
 export default {
+  components: { stayPreview },
   props: ["user"],
 
   computed:{
