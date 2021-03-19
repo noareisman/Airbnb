414c00fb00228b17efac7cf41f52ed72b6f83a7d
ReamLasry <ream794613@gmail.com>
2021-03-19 18:13:25 +0200

    update

--- src/cmps/user-orders.vue
+++ src/cmps/user-orders.vue
@@ -1,38 +1,48 @@
 <template>
-  <section class="orders">
+  <section>
     <h2>Pending / Accepted reservations:</h2>
-    <ul>
-      <li
-        v-for="order in orders.filter((order) => order.status === 'pending')"
-        :key="order.id"
-        class="order"
+    <section class="orders">
+      <h2
+        v-if="
+          !orders.filter((order) => order.status === 'pending') ||
+          !orders.filter((order) => order.status === 'pending').length
+        "
       >
-        <div class="order-bio">
-          <h2>{{ order.buyer.fullname }}</h2>
-          <p>{{ order.startDate }}-{{ order.endDate }}</p>
-        </div>
-        <div class="order-bio">
-          <h4>Stay at:&nbsp;</h4>
-          <p>"{{ order.stay.name }}"</p>
-        </div>
-        <div class="order-bio">
-          <h4>Guests:</h4>
-          <p>
-            Adults:&nbsp; {{ order.guests.adults }}, Kids:&nbsp;{{
-              order.guests.kids
-            }}
-          </p>
-        </div>
-        <div>
-          <button @click="changeOrderStat($event, order)" class="reject">
-            X
-          </button>
-          <button @click="changeOrderStat($event, order)" class="approve">
-            V
-          </button>
-        </div>
-      </li>
-    </ul>
+        No reservations to show...
+      </h2>
+      <ul>
+        <li
+          v-for="order in orders.filter((order) => order.status === 'pending')"
+          :key="order.id"
+          class="order"
+        >
+          <div class="order-bio">
+            <h2>{{ order.buyer.fullname }}</h2>
+            <p>{{ order.startDate }}-{{ order.endDate }}</p>
+          </div>
+          <div class="order-bio">
+            <h4>Stay at:&nbsp;</h4>
+            <p>"{{ order.stay.name }}"</p>
+          </div>
+          <div class="order-bio">
+            <h4>Guests:</h4>
+            <p>
+              Adults:&nbsp; {{ order.guests.adults }}, Kids:&nbsp;{{
+                order.guests.kids
+              }}
+            </p>
+          </div>
+          <div class="order-btns">
+            <button @click="changeOrderStat($event, order)" class="reject">
+              X
+            </button>
+            <button @click="changeOrderStat($event, order)" class="approve">
+              V
+            </button>
+          </div>
+        </li>
+      </ul>
+    </section>
   </section>
 </template>
 
