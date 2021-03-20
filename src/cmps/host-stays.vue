933e2c67479b99ac301900957b6535c91afb4cf6
ReamLasry <ream794613@gmail.com>
2021-03-20 03:20:28 +0200

    update

--- src/cmps/host-stays.vue
+++ src/cmps/host-stays.vue
@@ -1,48 +1,41 @@
 <template>
-  <ul>
-    <li v-for="stay in stays" :key="stay._id" class="user-stay">
-      <h2>{{ stay.name }}</h2>
-      <div class="user-stay-img">
-        <img
-          :src="require(`@/assets/imgs/airbnb-imgs/${stay.imgUrls[0]}.jpg`)"
-          alt="img not find"
-        />
-      </div>
-      <single-stay-order :stay="stay" />
-    </li>
-  </ul>
+  <section>
+    <hr />
+    <!--REAM: FIND NEW NAME -->
+    <h2>Stays:</h2>
+    <!--REAM: FIND NEW NAME -->
+
+    <ul class="host-stays">
+      <li v-for="stay in stays" :key="stay._id" class="host-stay-and-orders">
+        <div class="host-stay">
+          <div class="host-stay-img">
+            <img
+              :src="require(`@/assets/imgs/airbnb-imgs/${stay.imgUrls[0]}.jpg`)"
+              alt="img not find"
+            />
+          </div>
+          <h2>{{ stay.name }}</h2>
+        </div>
+
+        <single-stay-order :stay="stay" />
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
-import singleStayOrder from '../cmps/single-stay-order.vue';
+import singleStayOrder from "../cmps/single-stay-order.vue";
 
 export default {
   props: ["host"],
-  // data() {
-  //   return {
-  //     stays: null,
-  //   };
-  // },
-  computed:{
-    stays(){
-      return this.$store.getters.staysForDisplay
-    }
+  computed: {
+    stays() {
+      return this.$store.getters.staysForDisplay;
+    },
   },
-  methods: {
-    async loadStays() {
-      const stays = await this.$store.dispatch({
-        type: "loadStays",
-        filterBy: this.host,
-      });
-      // this.stays = stays;
-    }
+  components: {
+    singleStayOrder,
   },
-  created() {
-    this.loadStays();
-  },
-  components:{
-    singleStayOrder
-  }
 };
 </script>
 <style>
