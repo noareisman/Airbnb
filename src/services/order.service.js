56ed7a75ecc155cdf5069e4b653b1a6a8fb6cb72
Noa Reisman <noareisman10@gmail.com>
2021-03-22 19:05:21 +0200

    order http service added

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,37 +1,41 @@
 import { storageService } from './async-storage.service'
+import { utilService } from './util.service.js';
 import { httpService } from './http.service'
 
 const gOrders = require('../../data/airbnb.json')
 
-
-
 export const orderService = {
-    // login,
-    // logout,
-    // signup,
     query,
-    // getById,
-    // remove,
     save,
-    // getLoggedinUser,
-    getNewOrder
+    getNewOrder,
+    getById,
+    remove//not in use
+}
+
+function getById(id) {
+    return httpService.get(`order/${id}`)
+}
+
+function remove(id) {
+    return httpService.delete(`order/${id}`)
 }
 
 async function query() {
-    // return httpService.get(`order`)
-    let orders = await storageService.query('order')
-    if (!orders || !orders.length) {
-        orders = JSON.parse(JSON.stringify(gOrders.order))
-        storageService.save('order', orders)
-    }
-    return orders;
+    return httpService.get(`order`)
+    // let orders = await storageService.query('order')
+    // if (!orders || !orders.length) {
+    //     orders = JSON.parse(JSON.stringify(gOrders.order))
+    //     storageService.save('order', orders)
+    // }
+    // return orders;
 }
 async function save(order) {
-    return storageService.put('order', order);
-    // user = await httpService.put(`user/${user._id}`, user)
-
-    // Handle case in which admin updates other user's details
-    // if (getLoggedinUser()._id === user._id) _saveLocalUser(user)
+    // return storageService.put('order', order);
+    if (order._id) {
+        return httpService.put(`order/${order._id}`, order)
+    } else{
+    return httpService.post('order/', order)
+    }    
 }
 
 function getNewOrder() {
