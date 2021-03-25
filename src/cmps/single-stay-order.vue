f4526cd72017b1fe34bc6e7f25c6dd25ff213770
ReamLasry <ream794613@gmail.com>
2021-03-25 17:51:52 +0200

    update:

--- src/cmps/single-stay-order.vue
+++ src/cmps/single-stay-order.vue
@@ -1,27 +1,34 @@
 <template>
     <section class="stay-order-details">
       <div class="stay-order-headlines">
-          <div class="cell"><h3>Guest name</h3></div>
+          <div class="cell"><h3>Name</h3></div>
           <div class="cell"><h3>Guests</h3></div>
-          <div class="cell"><h3>Check in</h3></div>
-          <div class="cell"><h3>Check out</h3></div>
+          <div class="cell check"><h3>Check in</h3></div>
+          <div class="cell check"><h3>Check out</h3></div>
+          <div class="cell check-respons"><h3>Check in/out</h3></div>
       </div>
+
       <div class="stay-full-orders">
-        <div class="single-star-order" v-for="order in singleStayOrders" :key="order._id">
+        <div class="single-stay-order" v-for="order in singleStayOrders" :key="order._id">
           <div class="cell" v-if="order.status === 'approve'">
             <b>{{ order.buyer.fullname }}</b>
           </div>
           <div class="cell" v-if="order.status === 'approve'">
             <p>
-              Adults:&nbsp;{{ order.guests.adults }} | Kids: &nbsp;{{
+              Adults:&nbsp;{{ order.guests.adults }} <span class="super-line">|</span> Kids: &nbsp;{{
                 order.guests.kids
               }}
             </p>
           </div>
-          <div class="cell" v-if="order.status === 'approve'">
+          <div class="cell date" v-if="order.status === 'approve'">
             <p>{{ order.startDate }}</p>
           </div>
-          <div class="cell" v-if="order.status === 'approve'">
+          <div class="cell date" v-if="order.status === 'approve'">
+            <p>{{ order.endDate }}</p>
+          </div>
+          
+          <div class="cell dates-respons" v-if="order.status === 'approve'">
+            <p>{{ order.startDate }}</p>
             <p>{{ order.endDate }}</p>
           </div>
         </div>
